f2b784de4aa9d26983787fed5c1dfd5a9fd025b7
FedorVinecky <31040475+Fedor73@users.noreply.github.com>
2019-01-18 11:34:03 +0400

    structure

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
-import React from 'react'
-import { routes } from './routes';
-import { BrowserRouter as Router, Switch } from 'react-router-dom';
-import  Navigation  from './components/Navigation'
+import React from "react";
+import { routes } from "./routes";
+import { BrowserRouter as Router, Switch } from "react-router-dom";
+import Navigation from "./components/Navigation";
+import NotFound from "./components/NotFound";
 
 const App = () => {
   return (
     <Router>
       <React.Fragment>
-        <Navigation routes={routes.filter(route => route.isNavBar)}/>
+        <Navigation routes={routes.filter(route => route.isNavBar)} />
         <h1>роуты</h1>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/news" component={NewsContainer} />
+          <Route path="/login" component={LoginContainer} />
+          <PrivateRoute path="/profile" component={ProfileContainer} />
+          <Route component={NotFound} />
+        </Switch>
       </React.Fragment>
     </Router>
   );
